6673e710f983799da47de02c991220cfe463a21a
L1L3KS <whykolyaggcom2@gmail.com>
2024-01-13 21:55:30 +0200

    nothing

--- JS.js
+++ JS.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function () {
     var parallax = document.querySelector('.parallax');
 
@@ -46,53 +45,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Показываем первую страницу при загрузке страницы
     updatePage();
-=======
-document.addEventListener('DOMContentLoaded', function () {
-    var parallax = document.querySelector('.parallax');
-
-    window.addEventListener('scroll', function () {
-        var offset = window.pageYOffset;
-        parallax.style.transform = 'translateY(' + offset * 0.6 + 'px)';
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    let currentPage = 1;
-    const totalPages = 4;
-
-    const prevPageButton = document.getElementById("prevPage");
-    const nextPageButton = document.getElementById("nextPage");
-
-    prevPageButton.addEventListener("click", function () {
-        if (currentPage > 1) {
-            currentPage--;
-            updatePage();
-        }
-    });
-
-    nextPageButton.addEventListener("click", function () {
-        if (currentPage < totalPages) {
-            currentPage++;
-            updatePage();
-        }
-    });
-
-    function updatePage() {
-        // Скрываем все элементы
-        const allElements = document.querySelectorAll(".sdfr");
-        allElements.forEach(function (element) {
-            element.style.display = "none";
-        });
-
-        // Показываем элементы текущей страницы
-        const currentPageElements = document.querySelectorAll(".page-" + currentPage);
-        currentPageElements.forEach(function (element) {
-            element.style.display = "inline-block";
-        });
-    }
-
-    // Показываем первую страницу при загрузке страницы
-    updatePage();
->>>>>>> bb5a506c538ea1543ef8200a9c7075662519fcf7
 });
