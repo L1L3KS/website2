05bc9b398e878466c461370a0887c2136feec272
Luca <36565154+Grisha101@users.noreply.github.com>
2024-01-20 18:23:00 +0200

    fixed function wf

--- function wf () {.js
+++ function wf () {.js
@@ -5,66 +5,6 @@ function wf () {
     console.log(result)
 }
 
-function remind () {
-    alert ("Знижка 20% на квиток в один бік!")
-}
-
-setTimeout(remind, 2*60*1000)
-
-function remind(message) {
-    alert(message)
-}
-
-setTimeout(function (){remind("Повідомлення")}, 2*60*1000)
-
-send_btn.addEventListener('mouseout', make_colorful)
-
-
-
-
-
-function name(arg){
-    console.Log("Консоль каже: ", arg)
-    return arg+20
-}
-
-let number = 50
-console.Log(number+30)
-
-
-let val = "12"
-switch (+val>10) {
-    case 11:
-        console.log("Arg = 11!")
-    break;
-    case 12:
-        console.log("Arg = 12!")
-    break;
-    default:
-        console.log("Такої умови не було описано")
-}
-
-
-
-/* function remind(asdawd) {
-    alert (asdawd)
-}*/
-
-
-
-
-function remind() {
-    setTimeout(function(){alert("ura")} , 5000)
-}
-let krosivki = document.getElementById("vapormax")
-krosivki.addEventListener("mouseover", remind())
-function wf () {
-    let number1 = 5
-    let number2 = 10
-    let rusult = number1 + number2
-    console.log(result)
-}
-
 
 let x = 10;
 let y = "1";
@@ -92,6 +32,10 @@ console.log(t == s)
 
 console.log(x != 10) 
 
+console.log(x <= y)
+console.log(x >= y)
+console.log(x < y)
+console.log(x > y)
 
 console.log(x + y)
 console.log(x - y)
@@ -100,23 +44,61 @@ console.log(x^y)
 console.log(x/ y)
 
 
-if (x == y) {
+if (x > y) {
     console.log("X більший за Y")
 } else if (x < y) {
     console.log("Y більший за X")
-} else {
+} else{
     console.log("x дорівнює y")
 }
 
+
+
+
+x = input("Введіть своє число: ")
+
+switch(x){
+case 1:
+    console.log("Понеділок") 
+break;
+
+case 2:
+    console.log("Вівторок")
+break;
+
+case 3:
+    console.log("Середа")
+break;
+
+case 4:
+    console.log("Четвер")
+break;
+
+case 5:
+    console.log("П'ятниця")
+break;
+
+default:
+    console.log("Ти загубився в часі")
+
+}
+
+
+
+
+
+
 function oven(a, b){
-    t = 200
-    res = a * b + t
+    let t = 200
+    let res = a * b + t
     return res
 }
 
 let pie = oven(2, 3)
+console.log(pie) // 206
 
-console.log(pie)
+
+
 
 
 
@@ -132,11 +114,9 @@ function remind(message) {
 }
 
 setTimeout(function (){remind("Повідомлення")}, 2*60*1000)
+setInterval(function (){remind("Повідомлення")}, 30*60*1000)
 
 send_btn.addEventListener('mouseout', make_colorful)
-
-
-
 
 
 function name(arg){
@@ -149,7 +129,7 @@ console.Log(number+30)
 
 
 let val = "12"
-switch (+val>10) {
+switch (+4>10) {
     case 11:
         console.log("Arg = 11!")
     break;
@@ -174,4 +154,3 @@ function remind() {
 }
 let krosivki = document.getElementById("vapormax")
 krosivki.addEventListener("mouseover", remind())
-
